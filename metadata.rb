b40f62d345d6e66eb69ceb7726f754f96d32d4c0
Matt Mencel <mr-mencel@wiu.edu>
2014-03-06 15:33:45 -0600

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tsmith@limelight.com'
 license          'Apache 2.0'
 description      'Installs/Configures LDAP on RHEL using SSSD'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
+version          '0.1.5'
 
 %w{ redhat centos amazon scientific oracle }.each do |os|
   supports os
