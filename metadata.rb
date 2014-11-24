db8ea1e9227ca1a6198a6eff07051bd39955d00d
Tim Smith <tim@cozy.co>
2014-11-24 11:17:24 -0800

    Bump to 1.0.3 for development

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tsmithi84@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures LDAP on RHEL/Ubuntu using SSSD'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.2'
+version          '1.0.3'
 
 %w( redhat centos amazon scientific oracle ubuntu ).each do |os|
   supports os
