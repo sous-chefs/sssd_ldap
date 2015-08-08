d2ac6cd2e46472ffe1a0e481c542a29787a4af25
Tim Smith <tim@cozy.co>
2015-08-07 23:29:49 -0700

    Add more specs

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,8 +1,19 @@
 require 'chefspec'
 
-describe 'sssd_ldap::default' do
+describe 'sssd_ldap::default ubuntu 12.04' do
+  let(:runner) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '12.04') }
+  let(:chef_run) { runner.converge('sssd_ldap::default') }
+
+  it 'installs libsss-sudo0 if sudo_ldap attribute set' do
+    chef_run.node.set['sssd_ldap']['ldap_sudo'] = true
+    chef_run.converge('sssd_ldap::default')
+    expect(chef_run).to install_package 'libsss-sudo0'
+  end
+end
+
+describe 'sssd_ldap::default ubuntu 14.04' do
   let(:runner) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04') }
-  let(:chef_run) { runner.converge(described_recipe) }
+  let(:chef_run) { runner.converge('sssd_ldap::default') }
 
   it 'installs sssd' do
     expect(chef_run).to install_package 'sssd'
@@ -17,8 +28,23 @@ describe 'sssd_ldap::default' do
     expect(install).to do_nothing
   end
 
+  it 'installs libsss-sudo if ldap_sudo attribute set' do
+    chef_run.node.set['sssd_ldap']['ldap_sudo'] = true
+    chef_run.converge('sssd_ldap::default')
+    expect(chef_run).to install_package 'libsss-sudo'
+  end
+
   it 'starts sssd' do
     expect(chef_run).to start_service 'sssd'
+  end
+end
+
+describe 'sssd_ldap::default centos' do
+  let(:runner) { ChefSpec::SoloRunner.new(platform: 'centos', version: '6.6') }
+  let(:chef_run) { runner.converge('sssd_ldap::default') }
+
+  it 'installs authconfig' do
+    expect(chef_run).to install_package 'authconfig'
   end
 end
 
