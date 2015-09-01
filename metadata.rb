ac240aba3df9831ec22ccdf737deb07e643e6fca
Tim Smith <tsmith84@gmail.com>
2015-09-01 09:39:13 -0700

    Fix URLs

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'sssd_ldap'
 maintainer       'Tim Smith'
-maintainer_email 'tsmithi84@gmail.com'
+maintainer_email 'tsmith84@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures LDAP on RHEL/Ubuntu using SSSD'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
@@ -9,5 +9,5 @@ version          '1.0.3'
 %w( redhat centos amazon scientific oracle ubuntu debian ).each do |os|
   supports os
 end
-source_url 'https://github.com/tas50/sssd_ldap' if respond_to?(:source_url)
-issues_url 'https://github.com/tas50/sssd_ldap/issues' if respond_to?(:source_url)
+source_url 'https://github.com/tas50/chef-sssd_ldap' if respond_to?(:source_url)
+issues_url 'https://github.com/tas50/chef-sssd_ldap/issues' if respond_to?(:source_url)
