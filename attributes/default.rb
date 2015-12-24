e0228eccff0dfd7b65a5e791d5780cd0888e8456
Tim Smith <tsmith84@gmail.com>
2015-12-24 10:40:59 -0800

    Fix rubocop warning

--- attributes/default.rb
+++ attributes/default.rb
@@ -55,4 +55,3 @@ default['sssd_ldap']['sssd_conf']['max_id'] = '0'
 default['sssd_ldap']['ldap_sudo'] = false
 default['sssd_ldap']['ldap_autofs'] = false
 default['sssd_ldap']['ldap_ssh'] = false
-
