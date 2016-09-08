9f7fc19fefe116b59196a681b515382423524495
Tim Smith <tsmith@chef.io>
2016-09-07 20:57:02 -0700

    Remove chef 11 compat in the metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -12,4 +12,4 @@ end
 source_url 'https://github.com/chef-cookbooks/chef-sssd_ldap'
 issues_url 'https://github.com/chef-cookbooks/chef-sssd_ldap/issues'
 
-chef_version '>= 12.0' if respond_to?(:chef_version)
+chef_version '>= 12.0'
