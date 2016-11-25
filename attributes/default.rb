4092d1593b786ddddb747174bd207a63e7c89907
Tim Smith <tsmith@chef.io>
2016-11-25 15:08:39 -0800

    update copyright headers

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: sssd_ldap
+# Cookbook:: sssd_ldap
 # Attributes:: default
 #
-# Copyright 2013-2014, Limelight Networks, Inc.
+# Copyright:: 2013-2016, Limelight Networks, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
