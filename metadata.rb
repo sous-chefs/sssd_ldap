f9e6ee560023e5e6bff67279b811f8a3f782cc8d
Tim Smith <tsmith84@gmail.com>
2019-12-12 16:32:14 -0800

    Remove the long_description metadata
    This is unused now
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
 description      'Sets up SSSD for LDAP on Ubuntu and RHEL systems'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '5.0.0'
 
 %w(redhat centos amazon scientific oracle ubuntu debian).each do |os|
